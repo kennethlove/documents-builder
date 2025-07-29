ef3be6ce806db656b3512b81be9221fb6d73bb8a
Kenneth Love <11908+kennethlove@users.noreply.github.com>
2025-07-29 13:09:16 -0700

    Rename files to match commands

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,8 +1,8 @@
 pub mod export_fragments;
 pub mod health_checks;
 pub mod list_all;
-pub mod process_org;
+pub mod process_organization;
 pub mod process_repository;
-pub mod scan_org;
+pub mod scan_organization;
 pub mod serve_webhook;
 pub mod validate_repo_config;
